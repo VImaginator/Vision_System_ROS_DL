2928c24d8658d2ecf9f91611d0936546800982b3
VImaginator <vimaginator@proton.me>
2021-04-25 16:28:06 +0000

    A bunch of stuff

---
+++ drv_face_service/src/classifier.cpp
@@ -0,0 +1,185 @@
+
+#include "classifier.h"
+
+using caffe::Blob;
+using caffe::Net;
+using std::string;
+
+Classifier::Classifier()
+{
+  
+}
+
+Classifier::Classifier(const string& deploy_proto,
+                       const string& caffe_model,
+                       const int gpu_id,
+                       const bool do_train)
+  : caffe_model_(caffe_model)
+{
+  SetupNetwork(deploy_proto, caffe_model, gpu_id, do_train);
+}
+
+void Classifier::SetupNetwork(const string& deploy_proto,
+                              const string& caffe_model,
+                              const int gpu_id,
+                              const bool do_train) {
+#ifdef CPU_ONLY
+  printf("Setting up Caffe in CPU mode.");
+  caffe::Caffe::set_mode(caffe::Caffe::CPU);
+#else
+  printf("Setting up Caffe in GPU mode with ID: %d\n", gpu_id);
+  caffe::Caffe::SetDevice(gpu_id);
+  caffe::Caffe::set_mode(caffe::Caffe::GPU);
+#endif
+
+  if (do_train)
+    net_.reset(new Net<float>(deploy_proto, caffe::TRAIN));
+  else
+    net_.reset(new Net<float>(deploy_proto, caffe::TEST));
+
+  if (caffe_model != "NONE") {
+    net_->CopyTrainedLayersFrom(caffe_model_);
+  } else {
+    printf("Not initializing network from pre-trained model.");
+  }
+
+  Blob<float>* input_layer = net_->input_blobs()[0];
+
+  num_channels_ = input_layer->channels();
+
+  input_geometry_ = Size(input_layer->width(), input_layer->height());
+
+  // Load the binaryproto mean file.
+  SetMean();
+}
+
+void Classifier::SetMean() {
+  // Set the mean image.
+  mean_ = cv::Mat(input_geometry_, CV_32FC3, cv::Scalar(104, 117, 123));
+}
+
+void Classifier::Classify(cv::Mat image_in, int &id, float &trust) {
+  assert(net_->phase() == caffe::TEST);
+
+  // Reshape the input blobs to be the appropriate size.
+  Blob<float>* input_image = net_->input_blobs()[0];
+  input_image->Reshape(1, num_channels_,
+                       input_geometry_.height, input_geometry_.width);
+
+  // Forward dimension change to all layers.
+  net_->Reshape();
+
+  // Process the inputs so we can set them.
+  vector<Mat> image_channels;
+  WrapInputLayer(&image_channels);
+
+  Preprocess(image_in, &image_channels);
+
+  // Perform a forward-pass in the network.
+  float *loss = new float;
+  net_->ForwardPrefilled(loss);
+  //  std::cerr<< "Loss is: " << *loss << std::endl;
+
+  // Get the network output.
+  GetOutput(id, trust);
+}
+
+/* Wrap the input layer of the network in separate cv::Mat objects
+   (one per channel). This way we save one memcpy operation and we
+   don't need to rely on cudaMemcpy2D. The last preprocessing
+   operation will write the separate channels directly to the input layer. */
+void Classifier::WrapInputLayer(vector<Mat>* target_channels) {
+  Blob<float>* input_layer_target = net_->input_blobs()[0];
+
+  int target_width = input_layer_target->width();
+  int target_height = input_layer_target->height();
+  float* target_data = input_layer_target->mutable_cpu_data();
+  for (int i = 0; i < input_layer_target->channels(); ++i) {
+    cv::Mat channel(target_height, target_width, CV_32FC1, target_data);
+    target_channels->push_back(channel);
+    target_data += target_width * target_height;
+  }
+}
+
+void Classifier::Preprocess(const Mat& img, vector<Mat>* input_channels) {
+  // Convert the input image to the input image format of the network.
+  Mat sample;
+  if (img.channels() == 3 && num_channels_ == 1)
+    cvtColor(img, sample, CV_BGR2GRAY);
+  else if (img.channels() == 4 && num_channels_ == 1)
+    cvtColor(img, sample, CV_BGRA2GRAY);
+  else
+    sample = img;
+
+  // Convert the input image to the expected size.
+  Mat sample_resized;
+  if (sample.size() != input_geometry_)
+    cv::resize(sample, sample_resized, input_geometry_);
+  else
+    sample_resized = sample;
+
+  // Convert the input image to the expected number of channels.
+  Mat sample_float;
+  if (num_channels_ == 3)
+    sample_resized.convertTo(sample_float, CV_32FC3);
+  else
+    sample_resized.convertTo(sample_float, CV_32FC1);
+
+  // Subtract the image mean to try to make the input 0-mean.
+  Mat sample_normalized;
+  subtract(sample_float, mean_, sample_normalized);
+
+  // This operation will write the separate BGR planes directly to the
+  // input layer of the network because it is wrapped by the cv::Mat
+  // objects in input_channels.
+  cv::split(sample_normalized, *input_channels);
+
+  /*CHECK(reinterpret_cast<float*>(input_channels->at(0).data)
+        == net_->input_blobs()[0]->cpu_data())
+    << "Input channels are not wrapping the input layer of the network.";*/
+}
+
+void Classifier::GetOutput(int &id, float &trust) {
+  // Get the fc8 output features of the network (this contains the estimated bounding box).
+  GetFeatures("prob", id, trust);
+}
+
+void Classifier::GetFeatures(const string& feature_name, int &id, float &trust) {
+  //printf("Getting %s features\n", feature_name.c_str());
+
+  // Get a pointer to the requested layer.
+  const boost::shared_ptr<Blob<float> > layer = net_->blob_by_name(feature_name.c_str());
+
+  // Copy all elements in this layer to a vector.
+  const float* begin = layer->cpu_data();
+
+  vector<float> featureList;
+  featureList.resize(layer->offset(1));
+
+  for (int i = 0;i < layer->offset(1); i++)
+    featureList[i] = begin[i];
+
+  vector<int> top_ids = Argmax(featureList, featureList.size());
+  // only output the id with highest trust
+  id = top_ids[0];
+  trust = featureList[id];
+}
+
+bool PairCompare(std::pair<float, int>& lhs, std::pair<float, int>& rhs)
+{
+  return lhs.first > rhs.first;
+}
+
+/* Return the indices of the top N values of vector v. */
+vector<int> Classifier::Argmax(vector<float> v, int N) {
+  vector<pair<float, int> > pairs;
+  for (size_t i = 0; i < v.size(); ++i)
+    pairs.push_back(make_pair(v[i], i));
+  partial_sort(pairs.begin(), pairs.begin() + N, pairs.end(), PairCompare);
+
+  vector<int> result;
+  for (int i = 0; i < N; ++i)
+    result.push_back(pairs[i].second);
+  return result;
+}
+
---
+++ drv_face_service/src/classifier.h
@@ -0,0 +1,66 @@
+#ifndef CLASSIFIER_H
+#define CLASSIFIER_H
+
+#include <caffe/caffe.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <iosfwd>
+#include <memory>
+#include <string>
+#include <utility>
+#include <vector>
+
+
+using namespace std;
+using namespace cv;
+
+class Classifier
+{
+public:
+  Classifier();
+  
+  Classifier(const string& train_deploy_proto,
+             const string& caffe_model,
+             const int gpu_id,
+             const bool do_train);
+
+  void Classify(Mat image_in, int &id, float &trust);
+
+protected:
+  boost::shared_ptr<caffe::Net<float> > net_;
+
+  // Get the features corresponding to the output of the network.
+  void GetOutput(int &id, float &trust);
+
+  // Get the features in the network with the given name,
+  // and copy their values to the output.
+  void GetFeatures(const string& feature_name, int &id, float &trust);
+
+private:
+  // Set up a network with the architecture specified in deploy_proto,
+  // with the model weights saved in caffe_model.
+  void SetupNetwork(const string& deploy_proto,
+                    const string& caffe_model,
+                    const int gpu_id,
+                    const bool do_train);
+
+  // Set the mean input (used to normalize the inputs to be 0-mean).
+  void SetMean();
+
+  // Wrap the input layer of the network in separate cv::Mat objects
+  // (one per channel).
+  void WrapInputLayer(vector<Mat>* target_channels);
+
+  // Set the inputs to the network.
+  void Preprocess(const Mat& img, vector<Mat>* input_channels);
+
+  vector<int> Argmax(vector<float> v, int N);
+
+private:
+  // Folder containing the model weights.
+  string caffe_model_;
+  // Size of the input images.
+  Size input_geometry_;
+
+  // Number of image 
