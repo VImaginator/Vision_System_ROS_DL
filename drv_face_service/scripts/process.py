3bd692c61341ef49d1587877832b03791a2ae34d
VImaginator <vimaginator@proton.me>
2021-04-25 10:55:46 +0000

    qa: applied PMD/checkstyle/findbugs rules

---
+++ drv_face_service/scripts/process.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+# Make sure these 2 lines are in your ~/.bashrc:
+
+# export PYTHONPATH="/home/USER/py-faster-rcnn/lib:/home/USER/py-faster-rcnn/caffe-fast-rcnn/python:${PYTHONPATH}"
+# export DRV=/home/USER/catkin_ws/src/drv_package
+
+# Change 'U
