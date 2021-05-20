3ec711b0ed51783bdb6a22462bf4a7e3d260a8ab
VImaginator <vimaginator@proton.me>
2021-05-20 16:48:06 +0000

    Docstring fixes

--- drv_grasp/src/makeplan.cpp
+++ drv_grasp/src/makeplan.cpp
@@ -23,4 +23,62 @@ bool MakePlan::getAveragePoint(PointCloud::Ptr cloud_in, pcl::PointXYZ &avrPt)
   }
   
   pcl::PointXYZ min_dis_point;
-  float min_dis = 100.
+  float min_dis = 100.0;
+  for (PointCloud::const_iterator it = cloud_in->begin(); it != cloud_in->end(); ++ it)
+  {
+    float dis = pow(it->x, 2) + pow(it->y, 2);
+    if (dis < min_dis)
+    {
+      min_dis = dis;
+      min_dis_point = *it;
+    }
+  }
+  
+  smartOffset(min_dis_point, 0.02, 0.05);
+  avrPt = min_dis_point;
+  return true;
+}
+
+void MakePlan::removeNans(PointCloud::Ptr cloud_in, PointCloud::Ptr &cloud_out)
+{
+  std::vector< int >	index;
+  PointCloud::Ptr cloud_filtered_nan (new PointCloud);
+  
+  pcl::removeNaNFromPointCloud(*cloud_in, *cloud_filtered_nan, index);
+  
+  pcl::PointIndices::Ptr indices_x(new pcl::PointIndices);
+  pcl::PointIndices::Ptr indices_xy(new pcl::PointIndices);
+  
+  pcl::PassThrough<pcl::PointXYZ> ptfilter; // Initializing with true will allow us to extract the removed indices
+  ptfilter.setInputCloud (cloud_filtered_nan);
+  ptfilter.setFilterFieldName ("x");
+  ptfilter.setFilterLimits (-0.2, 0.2);
+  ptfilter.filter (indices_x->indices);
+  
+  ptfilter.setIndices (indices_x);
+  ptfilter.setFilterFieldName ("y");
+  ptfilter.setFilterLimits (-0.2, 0.2);
+  ptfilter.filter (indices_xy->indices);
+  
+  ptfilter.setIndices (indices_xy);
+  ptfilter.setFilterFieldName ("z");
+  ptfilter.setFilterLimits (0.5, 2.0);// this method can only be used on source cloud, aka cloud whose frame is camera optical frame
+  ptfilter.filter (*cloud_out);
+}
+
+bool MakePlan::process(PointCloud::Ptr cloud_in, float a, float b, float c, float d, pcl::PointXYZ &avrPt)
+{
+  if (!cloud_in->points.size())
+  {
+    return false;
+  }
+  
+  PointCloud::Ptr cloud_filtered (new PointCloud);
+#ifdef USE_CENTER
+  cloud_filtered = cloud_in;
+#else
+  removeOutliers(cloud_in, cloud_filtered);
+#endif
+  bool success = getAveragePoint(cloud_filtered, avrPt);
+  return success;
+}
---
+++ drv_grasp/src/obstacledetect.cpp
@@ -0,0 +1,20 @@
+﻿#include <tf2/LinearMath/Quaternion.h>
+#include "obstacledetect.h"
+
+
+// Normal threshold, |z_norm_| > th_z_norm_ means the point is from plane
+float th_z_norm_ = 0.7;
+
+// Region growing threshold
+float th_smooth_ = 8;
+
+// Voxel grid threshold
+float th_leaf_ = 0.015;
+// th_deltaz_ must 2 times bigger than th_leaf_
+float th_deltaz_ = 2 * th_leaf_;
+float th_ratio_ = 5 * th_leaf_; // flatness ratio max value of plane
+
+// Depth threshold
+float th_max_depth_ = 1.3;
+
+ObstacleDetect::ObstacleDetect(bo
