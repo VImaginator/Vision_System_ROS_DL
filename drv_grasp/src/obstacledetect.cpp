98987e5609a2de8337e5515ce8d1a7b58d2cd636
VImaginator <vimaginator@proton.me>
2021-06-13 06:43:04 +0000

    Update Server_AdvanceTurn.lua

--- drv_grasp/src/obstacledetect.cpp
+++ drv_grasp/src/obstacledetect.cpp
@@ -254,4 +254,115 @@ void ObstacleDetect::publishCloud(PointTPtr cloud, ros::Publisher pub)
   sensor_msgs::PointCloud2 ros_cloud;
   pcl::toROSMsg(*cloud, ros_cloud);
   ros_cloud.header.frame_id = base_frame_;
-  ros_cloud.header.stamp = ros::Time(0)
+  ros_cloud.header.stamp = ros::Time(0);
+  pub.publish(ros_cloud);
+}
+
+bool ObstacleDetect::analysePutPose(geometry_msgs::PoseStamped &put_pose,
+                                    geometry_msgs::PoseStamped &ref_pose)
+{
+  PointCloudMono::Ptr cloud = plane_max_hull_;
+  
+  pcl::PointXY p;
+  // Cause the camera view field is limited, we consider a hull is in reach
+  // when the center point plus a toletance in x direction is in hull
+  p.x = grasp_area_x_ + tolerance_;
+  p.y = grasp_area_y_;
+  
+  put_pose.header.frame_id = base_frame_;
+  put_pose.header.stamp = ros::Time(0);
+  ref_pose.header.frame_id = base_frame_;
+  ref_pose.header.stamp = ros::Time(0);
+  
+  PointCloudMono::Ptr cloud_sk(new PointCloudMono);
+  //  Utilities::shrinkHull(cloud, cloud_sk, 0.06);
+  publishCloud(cloud, pub_table_points_); // For reference
+  
+  pcl::PointXY p_dis;
+  pcl::PointXY p_closest;
+  if (Utilities::isInHull(cloud, p, p_dis, p_closest)) {
+    put_pose.pose.position.x = grasp_area_x_;
+    put_pose.pose.position.y = grasp_area_y_;
+    // All points in cloud have same z, so we directly use the first point
+    put_pose.pose.position.z = cloud->points[0].z + z_offset_;
+    put_pose.pose.orientation.x = 0;
+    put_pose.pose.orientation.y = 0;
+    put_pose.pose.orientation.z = 0;
+    put_pose.pose.orientation.w = 1;
+    
+    ROS_INFO_THROTTLE(11, "ObstacleDetect: Put pose detected.");
+    return false; // Return value means whether need move
+  }
+  else {
+    put_pose.pose.position.x = p_dis.x;
+    put_pose.pose.position.y = p_dis.y;
+    // All points in cloud have same z
+    put_pose.pose.position.z = cloud->points[0].z + z_offset_;
+    put_pose.pose.orientation.x = 0;
+    put_pose.pose.orientation.y = 0;
+    put_pose.pose.orientation.z = 0;
+    put_pose.pose.orientation.w = 1;
+    
+    ref_pose.pose.position.x = p_closest.x;
+    ref_pose.pose.position.y = p_closest.y;
+    ref_pose.pose.position.z = cloud->points[0].z + z_offset_;
+    ref_pose.pose.orientation.x = 0;
+    ref_pose.pose.orientation.y = 0;
+    ref_pose.pose.orientation.z = 0;
+    ref_pose.pose.orientation.w = 1;
+    ROS_INFO_THROTTLE(11, "ObstacleDetect: Need move to put object.");
+    return true;
+  }
+}
+
+void ObstacleDetect::analyseObstacle()
+{
+  PointCloudMono::Ptr cloud = plane_max_hull_;
+  if (cloud == NULL) {
+    ROS_WARN("ObstacleDetect: No table detected.");
+    return;
+  }
+  
+  // For now, we only publish the largest plane
+  pcl::PointXYZ minPt, maxPt;
+  pcl::getMinMax3D(*cloud, minPt, maxPt);
+  
+  // Calculate table centroid position
+  geometry_msgs::PoseStamped table_pose;
+  table_pose.header.frame_id = base_frame_;
+  table_pose.header.stamp = ros::Time(0);
+  table_pose.pose.position.x = (maxPt.x + minPt.x)/2;
+  table_pose.pose.position.y = (maxPt.y + minPt.y)/2;
+  table_pose.pose.position.z = (maxPt.z - base_link_above_ground_)/2;
+  
+  // Try find the longest line between neighbor points to be the edge
+  float dis_temp = 0.0;
+  float xs_, ys_, xe_, ye_;
+  float mean_x = (maxPt.x + minPt.x)/2;
+  for (size_t i = 0; i < cloud->points.size(); ++i) {
+    float xs = cloud->points[i].x;
+    float ys = cloud->points[i].y;
+    int ide = (i + 1) < cloud->points.size() ? (i + 1) : 0;
+    float xe = cloud->points[ide].x;
+    float ye = cloud->points[ide].y;
+    // Only consider the far edge
+    if (xs >= mean_x && xe >= mean_x) {
+      float dis = pow(xe - xs, 2) + pow(ye - ys, 2);
+      if (dis > dis_temp) {
+        dis_temp = dis;
+        xs_ = xs; ys_ = ys; xe_ = xe; ye_ = ye;
+      }
+    }
+  }
+  float yaw = 0.0;
+  // atan2 is better than atan in 2 ways:
+  // atan2 can handle the condition that the denominator=0
+  // atan2 in range -Pi~PI while atan in -PI/2~PI/2
+  // here the denominator plus -1 cause the coordinate of atan2 is
+  // different with that of ROS (with rotation=PI/2)
+  yaw = atan2(xe_ - xs_, ys_ - ye_);
+  
+  tf2::Quaternion q;
+  q.setEuler(0, 0, yaw); // Notice the last angle is around Z axis
+  table_pose.pose.orientation.x = q.x();
+  table_pose.pose
