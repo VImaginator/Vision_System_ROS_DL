52787bc26c556beeb9a88787eacc6a525ad75c2f
VImaginator <vimaginator@proton.me>
2021-09-12 06:19:20 +0000

    package upgrade

--- drv_recognize/src/refinedepth.h
+++ drv_recognize/src/refinedepth.h
@@ -11,4 +11,29 @@
 #include <string>
 #include <utility>
 #include <iostream>
-#incl
+#include <random>
+#include <vector>
+
+using namespace std;
+using namespace cv;
+
+class RefineDepth
+{
+public:
+  RefineDepth();
+  void refineDepth(Mat rgb_in, Mat depth_in, Mat &depth_out);
+
+protected:
+  boost::shared_ptr<caffe::Net<float> > net_;
+
+private:
+  void forward(vector<float> depth_in, vector<vector<float> > features_in,
+               vector<vector<float> > features_out, Mat &depth_out);
+  void setParams();
+  void wrapInputLayer(vector<float> depth_in, vector<vector<float> > features_in,
+                      vector<vector<float> > features_out);
+  void getOutput(Mat &depth_out);
+};
+
+#endif // REFINEDEPTH_H
+
