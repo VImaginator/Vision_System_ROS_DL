4672d5a865d8883132361b0cc8d104564063e037
VImaginator <vimaginator@proton.me>
2021-09-19 05:34:32 +0000

    add Vagrantfile

---
+++ drv_track/cmake/Modules/FindTinyXML.cmake
@@ -0,0 +1,8 @@
+##################################################################################################
+# 
+# CMake script for finding TinyXML.
+# 
+# Input variables:
+# 
+# - TinyXML_ROOT_DIR (optional): When specified, header files and libraries will be searched for in
+#     $
