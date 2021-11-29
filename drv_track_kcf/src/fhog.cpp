340432127f696e77789a1f90cb607347968dac68
VImaginator <vimaginator@proton.me>
2021-11-29 15:11:13 +0000

    Update Performance.php

--- drv_track_kcf/src/fhog.cpp
+++ drv_track_kcf/src/fhog.cpp
@@ -29,4 +29,142 @@
 // This software is provided by the copyright holders and contributors "as is" and
 // any express or implied warranties, including, but not limited to, the implied
 // warranties of merchantability and fitness for a particular purpose are disclaimed.
-// In no event shall the Intel Corpo
+// In no event shall the Intel Corporation or contributors be liable for any direct,
+// indirect, incidental, special, exemplary, or consequential damages
+// (including, but not limited to, procurement of substitute goods or services;
+// loss of use, data, or profits; or business interruption) however caused
+// and on any theory of liability, whether in contract, strict liability,
+// or tort (including negligence or otherwise) arising in any way out of
+// the use of this software, even if advised of the possibility of such damage.
+//
+//M*/
+
+
+//Modified from latentsvm module's "lsvmc_featurepyramid.cpp".
+
+//#include "precomp.hpp"
+//#include "_lsvmc_latentsvm.h"
+//#include "_lsvmc_resizeimg.h"
+
+#include "fhog.hpp"
+
+
+#ifdef HAVE_TBB
+#include <tbb/tbb.h>
+#include "tbb/parallel_for.h"
+#include "tbb/blocked_range.h"
+#endif
+
+#ifndef max
+#define max(a,b)            (((a) > (b)) ? (a) : (b))
+#endif
+
+#ifndef min
+#define min(a,b)            (((a) < (b)) ? (a) : (b))
+#endif
+
+
+/*
+// Getting feature map for the selected subimage
+//
+// API
+// int getFeatureMaps(const IplImage * image, const int k, featureMap **map);
+// INPUT
+// image             - selected subimage
+// k                 - size of cells
+// OUTPUT
+// map               - feature map
+// RESULT
+// Error status
+*/
+int getFeatureMaps(const IplImage* image, const int k, CvLSVMFeatureMapCaskade **map)
+{
+    int sizeX, sizeY;
+    int p, px, stringSize;
+    int height, width, numChannels;
+    int i, j, kk, c, ii, jj, d;
+    float  * datadx, * datady;
+    
+    int   ch; 
+    float magnitude, x, y, tx, ty;
+    
+    IplImage * dx, * dy;
+    int *nearest;
+    float *w, a_x, b_x;
+
+    float kernel[3] = {-1.f, 0.f, 1.f};
+    CvMat kernel_dx = cvMat(1, 3, CV_32F, kernel);
+    CvMat kernel_dy = cvMat(3, 1, CV_32F, kernel);
+
+    float * r;
+    int   * alfa;
+    
+    float boundary_x[NUM_SECTOR + 1];
+    float boundary_y[NUM_SECTOR + 1];
+    float max, dotProd;
+    int   maxi;
+
+    height = image->height;
+    width  = image->width ;
+
+    numChannels = image->nChannels;
+
+    dx    = cvCreateImage(cvSize(image->width, image->height), 
+                          IPL_DEPTH_32F, 3);
+    dy    = cvCreateImage(cvSize(image->width, image->height), 
+                          IPL_DEPTH_32F, 3);
+
+    sizeX = width  / k;
+    sizeY = height / k;
+    px    = 3 * NUM_SECTOR; 
+    p     = px;
+    stringSize = sizeX * p;
+    allocFeatureMapObject(map, sizeX, sizeY, p);
+
+    cvFilter2D(image, dx, &kernel_dx, cvPoint(-1, 0));
+    cvFilter2D(image, dy, &kernel_dy, cvPoint(0, -1));
+    
+    float arg_vector;
+    for(i = 0; i <= NUM_SECTOR; i++)
+    {
+        arg_vector    = ( (float) i ) * ( (float)(PI) / (float)(NUM_SECTOR) );
+        boundary_x[i] = cosf(arg_vector);
+        boundary_y[i] = sinf(arg_vector);
+    }/*for(i = 0; i <= NUM_SECTOR; i++) */
+
+    r    = (float *)malloc( sizeof(float) * (width * height));
+    alfa = (int   *)malloc( sizeof(int  ) * (width * height * 2));
+
+    for(j = 1; j < height - 1; j++)
+    {
+        datadx = (float*)(dx->imageData + dx->widthStep * j);
+        datady = (float*)(dy->imageData + dy->widthStep * j);
+        for(i = 1; i < width - 1; i++)
+        {
+            c = 0;
+            x = (datadx[i * numChannels + c]);
+            y = (datady[i * numChannels + c]);
+
+            r[j * width + i] =sqrtf(x * x + y * y);
+            for(ch = 1; ch < numChannels; ch++)
+            {
+                tx = (datadx[i * numChannels + ch]);
+                ty = (datady[i * numChannels + ch]);
+                magnitude = sqrtf(tx * tx + ty * ty);
+                if(magnitude > r[j * width + i])
+                {
+                    r[j * width + i] = magnitude;
+                    c = ch;
+                    x = tx;
+                    y = ty;
+                }
+            }/*for(ch = 1; ch < numChannels; ch++)*/
+            
+            max  = boundary_x[0] * x + boundary_y[0] * y;
+            maxi = 0;
+            for (kk = 0; kk < NUM_SECTOR; kk++) 
+            {
+                dotProd = boundary_x[kk] * x + boundary_y[kk] * y;
+                if (dotProd > max) 
+                {
+                    max  = 
