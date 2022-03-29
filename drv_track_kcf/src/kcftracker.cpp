f81102961028b66135e6773f57a2fb3f1a7c4028
VImaginator <vimaginator@proton.me>
2022-03-29 09:24:06 +0000

    first step

--- drv_track_kcf/src/kcftracker.cpp
+++ drv_track_kcf/src/kcftracker.cpp
@@ -7,4 +7,107 @@ KCF is implemented by using HOG features (the default), since it extends CSK to 
 [1] J. F. Henriques, R. Caseiro, P. Martins, J. Batista,
 "High-Speed Tracking with Kernelized Correlation Filters", TPAMI 2015.
 
-[2] J. F. Henriqu
+[2] J. F. Henriques, R. Caseiro, P. Martins, J. Batista,
+"Exploiting the Circulant Structure of Tracking-by-detection with Kernels", ECCV 2012.
+
+Authors: Joao Faro, Christian Bailer, Joao F. Henriques
+Contacts: joaopfaro@gmail.com, Christian.Bailer@dfki.de, henriques@isr.uc.pt
+Institute of Systems and Robotics - University of Coimbra / Department Augmented Vision DFKI
+
+
+Constructor parameters, all boolean:
+    hog: use HOG features (default), otherwise use raw pixels
+    fixed_window: fix window size (default), otherwise use ROI size (slower but more accurate)
+    multiscale: use multi-scale tracking (default; cannot be used with fixed_window = true)
+
+Default values are set for all properties of the tracker depending on the above choices.
+Their values can be customized further before calling init():
+    interp_factor: linear interpolation factor for adaptation
+    sigma: gaussian kernel bandwidth
+    lambda: regularization
+    cell_size: HOG cell size
+    padding: area surrounding the target, relative to its size
+    output_sigma_factor: bandwidth of gaussian target
+    template_size: template size in pixels, 0 to use ROI size
+    scale_step: scale step for multi-scale estimation, 1 to disable it
+    scale_weight: to downweight detection scores of other scales for added stability
+
+For speed, the value (template_size/cell_size) should be a power of 2 or a product of small prime numbers.
+
+Inputs to init():
+   image is the initial frame.
+   roi is a cv::Rect with the target positions in the initial frame
+
+Inputs to update():
+   image is the current frame.
+
+Outputs of update():
+   cv::Rect with target positions for the current frame
+
+
+By downloading, copying, installing or using the software you agree to this license.
+If you do not agree to this license, do not download, install,
+copy or use the software.
+
+
+                          License Agreement
+               For Open Source Computer Vision Library
+                       (3-clause BSD License)
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+  * Redistributions of source code must retain the above copyright notice,
+    this list of conditions and the following disclaimer.
+
+  * Redistributions in binary form must reproduce the above copyright notice,
+    this list of conditions and the following disclaimer in the documentation
+    and/or other materials provided with the distribution.
+
+  * Neither the names of the copyright holders nor the names of the contributors
+    may be used to endorse or promote products derived from this software
+    without specific prior written permission.
+
+This software is provided by the copyright holders and contributors "as is" and
+any express or implied warranties, including, but not limited to, the implied
+warranties of merchantability and fitness for a particular purpose are disclaimed.
+In no event shall copyright holders or contributors be liable for any direct,
+indirect, incidental, special, exemplary, or consequential damages
+(including, but not limited to, procurement of substitute goods or services;
+loss of use, data, or profits; or business interruption) however caused
+and on any theory of liability, whether in contract, strict liability,
+or tort (including negligence or otherwise) arising in any way out of
+the use of this software, even if advised of the possibility of such damage.
+ */
+
+#ifndef _KCFTRACKER_HEADERS
+#include "kcftracker.hpp"
+#include "ffttools.hpp"
+#include "recttools.hpp"
+#include "fhog.hpp"
+#include "labdata.hpp"
+#endif
+
+// Constructor
+KCFTracker::KCFTracker(bool hog, bool fixed_window, bool multiscale, bool lab)
+{
+  // Parameters equal in all cases
+  lambda = 0.0001;
+  padding = 2.5;
+  //output_sigma_factor = 0.1;
+  output_sigma_factor = 0.125;
+
+  if (hog) {
+    // VOT
+    interp_factor = 0.012;
+    sigma = 0.6;
+    // TPAMI
+    //interp_factor = 0.02;
+    //sigma = 0.5;
+    cell_size = 4;
+    _hogfeatures = true;
+
+    if (lab) {
+      interp_factor = 0.005;
+      sigma = 0.4;
+      //output_sigma
